d6f0036458cb317382d8bd4e107e1c3cb0c4b72c
03IJC <cook.isaac.2003@gmail.com>
2026-03-10 19:01:54 -0700

    Added logging into device api

--- Backend/src/api/device.py
+++ Backend/src/api/device.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import Session
 from ..core.dependencies import require_device
 from ..core.exceptions import NotFoundError, ValidationError
 from ..database.session import get_db
+from ..models.log import EventType
 from ..schemas.device import *
 from ..services.access_service import AccessService
 from ..services.lock_service import LockService
+from ..services.log_service import LogService
 
 
 router = APIRouter(prefix = "/device", tags = ["Device"])
@@ -19,6 +21,11 @@ def lock_heartbeat(
     db: Session = Depends(get_db)
 ):
     LockService(db).heartbeat(lock_id, data.status)
+    LogService(db).log(
+        event_type = EventType.LOCK_HEARTBEAT,
+        success = True,
+        lock_id = lock_id,
+    )
 
 
 @router.post("/access", response_model = AccessResponse)
@@ -29,8 +36,21 @@ def lock_access_attempt(
 ):
     try:
         LockService(db).get_lock_by_id(data.lock_id)
-        AccessService(db).verify_fingerprint(data.template_id)
+        fingerprint = AccessService(db).verify_fingerprint(data.template_id)
+
+        LogService(db).log(
+            event_type = EventType.UNLOCK_SUCCESS,
+            success = True,
+            lock_id = data.lock_id,
+            fingerprint_id = fingerprint.id
+        )
 
         return AccessResponse(granted=True)
     except (NotFoundError, ValidationError):
+        LogService(db).log(
+            event_type = EventType.UNLOCK_FAILURE,
+            success = False,
+            lock_id = data.lock_id,
+        )
+
         return AccessResponse(granted=False)
